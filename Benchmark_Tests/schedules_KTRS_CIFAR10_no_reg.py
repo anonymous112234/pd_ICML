cf7552e45ff606044148e3a01c64b3209c82183d
Anonymous112234 <abhinav_pomalapally@berkeley.edu>
2024-01-20 21:16:12 -0800

    CIFAR10 schedule random search test

--- Benchmark_Tests/schedules_KTRS_CIFAR10_no_reg.py
+++ Benchmark_Tests/schedules_KTRS_CIFAR10_no_reg.py
@@ -59,7 +59,7 @@ def build_model(hp):
 
 	# hp_reg = hp.Float("reg_term", min_value=1e-5, max_value=1e-1)
 
-	model.add(tf.keras.layers.Dense(256, activation = "relu", kernel_regularizer=tf.keras.regularizers.l2(l=hp_reg)))
+	model.add(tf.keras.layers.Dense(256, activation = "relu"))
 	model.add(tf.keras.layers.Dense(10, activation = "softmax"))
 
 
